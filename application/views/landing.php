86871c5776a55a8ee3881cba728bafa058f5601a
carlodeguzman5 <carlodeguzman5@gmail.com>
2015-12-27 14:43:52 +0800

    Initial Commit

---
+++ application/views/landing.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to Jammer</title>
+	  
+	  <!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.4/css/materialize.min.css">
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.4/js/materialize.min.js"></script>
+
+</head>
+<body>
+
+<div class="card" id="container">
+    <h1>Welcome to Jammer!</h1>
+
+    <div id="body">
+
+    </div>
+</div>
+
+</body>
+</html>
